fe4b985cd2dcb54aff26055bb29eb752019c27d9
DevNathJoydeep <devnathjoydeep@gmail.com>
2018-09-30 01:50:51 +0530

    thirdCommit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,39 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  $("#nav").click(function() {
+    $(this).toggleClass("open");
+    $(".navOverlay").fadeToggle(400, "swing");
+  });
 
-	$('#nav').click(function(){
-        $(this).toggleClass('open');
-        $('.navOverlay').fadeToggle( 400, "swing" );
-    });
-
+  $("a[class*='scroll']").on("click", function(event) {
+    $("#nav").toggleClass("open");
+    $(".navOverlay").fadeToggle(400, "swing");
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+  $("a[class*='musicScroll']").on("click", function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
 });
